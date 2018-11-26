f0ac7cd3928cd9b0b3a1856b59e02866eebbae46
Robert Schulte <schulte.robertw@outlook.com>
2018-11-26 14:40:07 -0600

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+// document.getElementById('main')
+//   .addEventListener('click', function(event) {
+//   return alert('I was clicked!')
+// })
